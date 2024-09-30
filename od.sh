fec6841889fdc47936bf46d4c63a02e924dd8208
bo zhang <maxutility2011@gmail.com>
2024-09-30 14:42:11 -0400

    Basic object detection functions done

---
+++ od.sh
@@ -0,0 +1,5 @@
+rm -rf ./inputs ./outputs #2>/dev/null
+mkdir ./inputs #2>/dev/null
+ffmpeg -i $1 -vf fps=$2 ./inputs/image_%4d.png
+python3 yolo.py --input_folder=./inputs/ --output_folder=./outputs/
+ffmpeg -hide_banner -loglevel error -r $2 -i ./outputs/image_%4d.jpg -vcodec libx264 -preset veryfast -crf 25 -y ./output.mp4
--- yolo.py
+++ yolo.py
@@ -11,7 +11,7 @@ parser = argparse.ArgumentParser(description='Small object detection with Yolov8
 parser.add_argument('--loglevel', type=str, default='INFO', help='Set the log level (e.g., DEBUG, INFO, WARNING, ERROR, CRITICAL)')
 #parser.add_argument('--trt_engine', type=str, required=True, default='./real_esrgan.engine', help='Path to the vsr inference engine file')
 parser.add_argument('--input_folder', type=str, required=True, help='The top level input folder that contains all the input images')
-parser.add_argument('--output_folder', type=str, required=False, help='The output folder')
+parser.add_argument('--output_folder', type=str, required=True, help='The output folder')
 
 args = parser.parse_args()
 
@@ -34,15 +34,21 @@ if len(input_images) == 0:
     logger.error("Failed to read images from %s", dirpath)
     exit(0)
 
+output_folder = args.output_folder
+os.makedirs(output_folder, exist_ok=True)
+
 for idx, img in enumerate(input_images):
     input_img = cv2.resize(img, (1280, 1280))
     results = model(input_img, conf=0.25)
     annotated_img = results[0].plot()
 
     # Display the result
-    plt.imshow(cv2.cvtColor(annotated_img, cv2.COLOR_BGR2RGB))
-    plt.axis('off')
-    plt.show()
+    #plt.imshow(cv2.cvtColor(annotated_img, cv2.COLOR_BGR2RGB))
+    #plt.axis('off')
+    #plt.show()
+
+    url = output_folder + "/image_" + ("%04d" % (idx+1)) + ".jpg"
+    cv2.imwrite(url, annotated_img)
 
     # If you want to save the result
     #cv2.imwrite('output_image.jpg', annotated_img)
