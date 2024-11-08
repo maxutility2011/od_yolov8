4b773d6f0e45f8fa8f160f8e3c99fa1bdb6b618b
Bo Zhang <maxutility2011@gmail.com>
2024-11-07 19:19:21 -0500

    integration test 3

--- od.sh
+++ od.sh
@@ -1,5 +1,6 @@
 rm -rf /tmp/inputs /tmp/outputs 2>/dev/null
 mkdir /tmp/inputs 2>/dev/null
+export FFREPORT=file=/tmp/ffmpeg.log:level=32
 ffmpeg -i $1 -vf fps=$2 /tmp/inputs/image_%4d.png -report
 python3 /home/streamer/bins/yolo.py --input_folder=/tmp/inputs/ --output_folder=/tmp/outputs/
 ffmpeg -hide_banner -loglevel error -r $2 -i /tmp/outputs/image_%4d.jpg -vcodec libx264 -preset veryfast -crf 25 -y /tmp/output.mp4 -report
